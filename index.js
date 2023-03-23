d97f87dfb849a4238bef5a40f711d7940a149e00
yamiwamiyu <10332709+yamiwamiyu@user.noreply.gitee.com>
2023-03-23 10:23:15 +0800

    1. 默认导出的vuex对象改为包含了所有createStore创建的对象 2. 增加了p(persistence)声明可默认通过localStorage持久化状态 3. 修复了外部set优先于get时，默认值又覆盖了外部set值的BUG 4. 修复了默认值异步方法和异步结果的BUG 5. 优化了set相同的值不重新赋值，先调用自定义set再真正赋值 6. createStore增加name参数方便创建多个仓库

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 import { ref, reactive, computed } from 'vue';
 
 /** 最后创建的vuex实例 */
-export let vuex;
+export const vuex = {
+  install(vue) {
+    for (const key in vuex)
+      vuex[key].install?.(vue);
+  }
+};
 
 /** 创建一个vuex实例 */
-export function createStore(option) {
+export function createStore(option, name = "$vuex") {
+  // 持久化数据
+  const pdatas = [];
   const x = reactive({});
   for (const key in option) {
     const value = option[key];
     let get, set;
     // 默认值的数组
     const __default = [];
+    // 是否持久化
+    let p;
 
     if (value) {
       if (value.constructor == Function) {
@@ -29,9 +38,14 @@ export function createStore(option) {
             __default.push(value);
           }
         }
+      } else if (value == localStorage) {
+        // 直接key: localStorage代表要持久化这个状态，其它默认
+        p = true;
       } else if (value.constructor == Object) {
         if (value.get || value.set || value.default) {
-          // vuex结构
+          // ee-vuex结构
+          if (value.p)
+            p = true;
           if (value.get)
             get = value.get;
           if (value.set)
@@ -58,17 +72,22 @@ export function createStore(option) {
       }
     }
 
+    // get设置默认值时，set不清空默认值
+    let setDefaultValue = false;
     // 默认值为undefined
     const v = ref(undefined);
     x[key] = computed({
       get: function () {
         // 设置默认值
         while (__default.length) {
+          setDefaultValue = true;
           const d = __default.shift();
           if (d) {
-            if (d.constructor == Function) {
+            if (d.constructor == Function || d.constructor?.name == 'AsyncFunction' || d.constructor == Promise) {
               // promise则等待异步结束
-              const dret = d.call(x);
+              let dret = d;
+              if (d.constructor == Function || d.constructor?.name == 'AsyncFunction')
+                dret = d.call(x);
               if (dret && dret.constructor == Promise) {
                 dret.then(i => x[key] = i);
               } else {
@@ -82,6 +101,7 @@ export function createStore(option) {
             x[key] = d;
           }
         }
+        setDefaultValue = false;
 
         let ret = v.value;
         // 允许get的返回值覆盖原本应该设置的值
@@ -93,22 +113,55 @@ export function createStore(option) {
         return ret;
       },
       set: function (value) {
-        // 调用set比调用get还要先的话，忽略掉get的默认值
-        // __default.length = 0;
-        v.value = value;
+        // 相同的值不重新赋值
+        if (v.value === value)
+          return;
+        // 外部调用set比调用get还要先的话，忽略掉get的默认值
+        if (!setDefaultValue)
+          __default.length = 0;
+        // 持久化
+        if (p) {
+          if (value != null)
+            localStorage.setItem(key, JSON.stringify(value));
+          else
+            localStorage.removeItem(key);
+        }
         // 允许set的返回值覆盖原本应该设置的值
         if (set) {
           const temp = set.call(x, value);
           if (temp)
-            v.value = temp;
+            value = temp;
         }
+        v.value = value;
       }
     })
+
+    // 还原持久化的值
+    if (p) {
+      // 先放入数组，等整个store声明完成后再赋值，防止赋值时触发set会引起store中其它状态变化
+      pdatas.push(() => {
+        const pdata = JSON.parse(localStorage.getItem(key));
+        if (pdata) {
+          // 有值就赋值并且清空__default
+          x[key] = pdata;
+          __default.length = 0;
+        }
+      })
+    }
   }
 
-  x.install = (vue) => { vue.config.globalProperties.$vuex = x; }
+  // 此时store已经声明完毕，还原持久化的值触发set引起其它state变化也没有问题
+  for (const p of pdatas)
+    p();
 
-  vuex = x;
+  if (name) {
+    if (vuex[name]) {
+      console.error("The vuex object already contains a warehouse named", name)
+    } else {
+      x.install = (vue) => { vue.config.globalProperties[name] = x; }
+      vuex[name] = x;
+    }
+  }
   return x;
 }
 
