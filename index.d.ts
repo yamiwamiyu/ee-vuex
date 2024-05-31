799902e30f4ed2b6f6bfabb0b8a079be45596e45
yamiwamiyu <10332709+yamiwamiyu@user.noreply.gitee.com>
2024-05-31 17:52:18 +0800

    injectStore 的 TS 类型完成

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-import { EmitsOptions, ComponentOptionsMixin, ComputedOptions, MethodOptions, DefineComponent, ComponentPropsOptions, ComponentOptionsWithObjectProps, Prop } from 'vue';
+import { EmitsOptions, ComponentOptionsMixin, ComputedOptions, MethodOptions, DefineComponent, ComponentOptionsWithObjectProps, Prop, ExtractPropTypes } from 'vue';
 
 /** 给一个仓库属性赋值后的回调
  * @param key - 赋值的属性
@@ -17,26 +17,25 @@ declare type StoreProperty<T = any> =
   StoreObject<T>
   // 注意：0 个参数和 void 返回值将导致这个属性显示为 () => void 函数，其实类型为 any
   // 1 或 2 参函数代表 set
-  | ((value: T, ...args: any[]) => void)
+  | ((value: T, set: (value: any) => any) => void)
   // 0 或 3 参以上代表 get
   | ((...args: any[]) => Promise<T> | T | void)
   // 0 参函数或任意类型
   | T;
 
-// todo: get 或 set 中的 value 如果只设置 T 类型，虽然能正确识别出类型，但是会导致很多情况的其它简写属性类型变为 unknown
-// 测试方法：将 get 的 T | any 改为 T，可以发现以下代码 value 为 number，但 unknown 为 unknown 类型
-// createStore({ hasGet: { init: 0, get(value) {} }, unknown() { return 5 } })
-export declare interface StoreObject<T = any> {
+// todo: get 或 set 中的 value 推断出了 T 类型，可是实际写调用 value 却又被识别为 any
+export declare interface StoreObject<T> {
   /** 是否持久化到localStorage，持久化的key默认为当前属性名 */
   p?: Boolean | Number;
   /** 获取属性值 */
-  get?: ((value: T | any) => (Promise<T> | T | void)),
+  get?: ((value: T) => Promise<T> | T | void),
   /** 设置属性值 */
-  set?: (value: T | any, set: (value: any) => void) => (Promise<T> | T | void);
+  set?: (value: T, set: (value: T) => void) => Promise<T> | T | void;
   /** 简单默认值，不支持异步，当 default 异步时作为返回前的值使用 */
   init?: T,
   /** 默认值，可以使用异步方法或返回 Promise 异步获取默认值 */
-  default?: (() => Promise<T> | T) | T;
+  default?: (() => Promise<T> | T) | Promise<T> | T;
+
   /** get/set/default 是否允许异步
    * 
    * 默认返回 Promise 类型会视为 Promise.resolve 的值作为返回值
@@ -61,9 +60,19 @@ export declare interface StoreObject<T = any> {
 declare type StoreProps<P = Data> = {
   [K in keyof P]: Prop<P[K]> | StoreProperty<P[K]> | null;
 };
-declare type ToNullValue<T extends Record<string, unknown>> = {
-  [K in keyof P]: null;
-}
+declare type ExtractStoreObject<T> = T extends { init: infer U } ? U
+  : T extends { default: infer U } ? (U extends Promise<infer V> ? V : U)
+  : T extends { get: (value: infer U) => any | void } ? U | any
+  : T extends { get: (...args: any[]) => R } ? R
+  : T extends { set: (value: infer U, ...args: any[]) => infer R } ? U | R | unknown
+  : T;
+declare type ExtractStoreProperty<T> = T extends object ? ExtractStoreObject<T>
+  : T extends (value: infer U, ...args: any[]) => any ? U
+  : T extends () => infer U ? U
+  : T;
+declare type ExtractStore<T> = T extends object ? {
+  [K in keyof T]: ExtractStoreProperty<T[K]>
+} : never;
 
 /**
  * 创建一个针对vue组件props的ee-vuex仓库
@@ -77,14 +86,15 @@ declare type ToNullValue<T extends Record<string, unknown>> = {
  * @param o - 选项式定义的Vue组件
  * @example injectStore({ props: { modelValue: 0, } })
  */
-export declare function injectStore<PropsOptions extends Readonly<StoreProps>, RawBindings, D, C extends ComputedOptions = {}, M extends MethodOptions = {}, Mixin extends ComponentOptionsMixin = ComponentOptionsMixin, Extends extends ComponentOptionsMixin = ComponentOptionsMixin, E extends EmitsOptions = Record<string, any>, EE extends string = string>(options: ComponentOptionsWithObjectProps<StoreProps<PropsOptions>, RawBindings, D, C, M, Mixin, Extends, E, EE>): DefineComponent<Array<keyof PropsOptions>, RawBindings, D, C, M, Mixin, Extends, E, EE>;
+export declare function injectStore<PropsOptions extends Readonly<StoreProps>, RawBindings, D, C extends ComputedOptions = {}, M extends MethodOptions = {}, Mixin extends ComponentOptionsMixin = ComponentOptionsMixin, Extends extends ComponentOptionsMixin = ComponentOptionsMixin, E extends EmitsOptions = Record<string, any>, EE extends string = string>(options: ComponentOptionsWithObjectProps<StoreProps<PropsOptions>, RawBindings, D, C, M, Mixin, Extends, E, EE, ExtractPropTypes<PropsOptions> | ExtractStoreProperty<PropsOptions>>): DefineComponent<Array<keyof PropsOptions>, RawBindings, D, C, M, Mixin, Extends, E, EE>;
 
 /** 创建一个ee-vuex仓库 */
-export declare function createStore<T, This = StoreInstance<T>>(store: ThisType<This> & Store<T>, option?: {
+export declare function createStore<T, R = Readonly<ExtractStore<T>>, This = R>(store: ThisType<This> & Store<T>, option?: {
   /** 仓库名，设置仓库名可以全局通过 const { 仓库名 } = require('ee-vuex').default 获得仓库实例 */
   name?: String;
   /** 仓库定义的 get/set/默认值 方法调用时的this实例，默认为仓库实例 */
   this?: This;
   /** 仓库中所有属性赋值后的回调方法，可用于记录日志等调试业务 */
   set?: OnStoreSet<T>;
-} | String): StoreInstance<T>;
+} | String) : R;
+//: ExtractStore<T>;
--- test.js
+++ test.js
@@ -3,26 +3,50 @@ import { injectStore, createStore } from "."
 injectStore({
     data() {
         return {
+            /** 注释a */
             a: 'a',
             b: 0,
         }
     },
     props: {
+        /** 测试 ee-vuex 属性 */
         test: {
             init: 5,
         },
+        /** 测试 vue 属性 */
         test2: String,
     },
     mounted() {
-        this.test
+        this.test2
     }
 })
 
 createStore({
-    hasGet: {
-        init: 0,
-        get(value) {
-        }
+    nulGet: {
+        get() {}
     },
-    unknown() { return '' }
+    hasGet: {
+        init: '',
+        set(value) {
+        },
+    },
+    unknown() { return '' },
+    test1() { return Date.now() },
+    /**
+     * @param {string} a 
+     */
+    test2(a, b) {
+    },
+    obj: {
+        a: 0,
+        b: '',
+        c: [],
+        d: {},
+        e() { return Date.now(); },
+        f: () => Date.now().toFixed(2),
+    },
+    actions: {
+        /** @param {number} a  */
+        power(a) { return a * a; },
+    },
 })
